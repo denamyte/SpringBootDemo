fc211d2c6dd7e02ce6a2a17e51fca80f0c99d7f1
denamyte <denamyte@gmail.com>
2020-12-23 00:58:38 +0200

    Hyperskill: Rest controller Web calculator :: finished

---
+++ src/main/java/com/denamyte/example/demo/todo/calculator/ArithmeticRestController.java
@@ -0,0 +1,28 @@
+package com.denamyte.example.demo.todo.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+
+@RestController
+public class ArithmeticRestController {
+
+    private Map<String, BiFunction<Integer, Integer, Long>> operations = Map.of(
+            "add", (a, b) -> (long) a + b,
+            "subtract", (a, b) -> (long) a - b,
+            "mult", (a, b) -> (long) a * b
+    );
+
+    @GetMapping(path = "/calc/{operation}")
+    public String calculate(@PathVariable String operation,
+                            @RequestParam int a,
+                            @RequestParam int b) {
+        return operations.containsKey(operation)
+                ? "" + operations.get(operation).apply(a, b)
+                : "Unknown operation";
+    }
+}

2c9c3c3f80398fe3fa51280046e9902d7fcad165
denamyte <denamyte@gmail.com>
2021-10-16 14:31:35 +0300

    Hyperskill: Problem 15218: Handling requests with bodies -> Important message

--- src/main/java/com/denamyte/example/demo/tasks/_15218/Controller15218.java
+++ src/main/java/com/denamyte/example/demo/tasks/_15218/Controller15218.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+/**
+ * @see <a href="https://hyperskill.org/learn/step/15218">Handling requests with bodies -> Important message</a>
+ */
 @RestController
 @RequestMapping("/15218")
 public class Controller15218 {
